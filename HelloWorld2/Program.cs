c3e327e333940a411cb7f50529662ee741f9dc46
Ovidiu Radoi <oradoi@talentsoft.com>
2019-11-28 17:36:02 +0200

    A new commit

--- HelloWorld2/Program.cs
+++ HelloWorld2/Program.cs
@@ -13,6 +13,7 @@ namespace HelloWorld2
             Console.WriteLine("Hello world");
             Console.WriteLine("Second line of code");
             Console.WriteLine("Third line of code");
+            Console.WriteLine("Fourth line of code");
         }
     }
 }
